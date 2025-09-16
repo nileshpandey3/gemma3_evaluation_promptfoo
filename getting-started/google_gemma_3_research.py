69f2ad8e2a10fd407d21545e979c63d81be16730
Nilesh Pandey <nileshpandey3@gmail.com>
2025-09-15 22:55:46 -0700

    cleanup and add doc string to the call_api()

--- getting-started/google_gemma_3_research.py
+++ getting-started/google_gemma_3_research.py
@@ -1,6 +1,13 @@
 from transformers import pipeline
 
+
 def call_api(prompt:str, options, context):
+    """
+    https://www.promptfoo.dev/docs/providers/python/
+    Simple python code to return an output from your custom model/logic
+    promptfoo uses this call_api() to receive output and continue its evaluation
+    """
+    
     pipe = pipeline("text-generation", model="google/gemma-3-270m-it")
     messages = [
         {"role": "user", "content": 
